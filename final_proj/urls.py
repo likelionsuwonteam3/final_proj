78764cf4c7dc03d7fc2745643f3ddfd913b325f6
kimseungki94 <skkim0832@gmail.com>
2019-02-27 00:15:17 +0900

    setting url and base html

--- final_proj/urls.py
+++ final_proj/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('detail/', views.detail, name='detail'),
-    path('blog/<int:blog_id>/',views.more, name='more'),
-    path('blog/new/', views.new, name='new'),
-    path('blog/create/', views.create, name='create'),
+    path('blog/',include('main.urls')),
 
 ]
